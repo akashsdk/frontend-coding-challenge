7179492b82a807f14905ce6b04509cede6bca818
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-11-26 17:19:00 +0600

    Update userAgent.tsx

--- src/views/userAgent/userAgent.tsx
+++ src/views/userAgent/userAgent.tsx
@@ -1,24 +1,28 @@
 "use client";
 
 import { BackToHome } from "@/components/backToHome/backToHome";
-import { useUserAgentContext } from "@/components/providers/userAgentProvider";
+import { useEffect, useState } from "react";
 
-export const UserAgent = () => {
-  const { userAgent } = useUserAgentContext();
+export const UserAgent = ({ userAgent: serverUserAgent }: { userAgent?: string }) => {
+  const [userAgent, setUserAgent] = useState<string | undefined>(serverUserAgent);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setUserAgent(navigator.userAgent); 
+    }
+  }, []);
 
   return (
     <div>
       <BackToHome />
-
-      {userAgent && (
+      {userAgent ? (
         <div className="flex font-mono font-semibold text-sm">
           <div className="border p-2">UserAgent</div>
-
           <div className="border p-2">{userAgent}</div>
         </div>
+      ) : (
+        <div>No user agent</div>
       )}
-
-      {!userAgent && <div>No user agent</div>}
     </div>
   );
 };
